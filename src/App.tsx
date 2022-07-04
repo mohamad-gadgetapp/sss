e7a541a95f977cdc1ceb5e54d2b4a10d56acfe29
mohammad <mohamad.gadgetapp@gmail.com>
2022-07-04 11:38:29 +0530

    add router in sss

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
-import { AgGridReact } from 'ag-grid-react';
+/** component */
 import Header from "./components/Header";
-import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-balham.css';
+
+
+
+/** pages */
+import HomePage from './pages/Home';
 
 function App() {
-  const [columnDefs, setColumnDefs] = React.useState<Array<object>>([
-    { headerName: "Make", field: "make" },
-    { headerName: "Model", field: "model" },
-    { headerName: "Price", field: "price" },
-  ]);
-  const [rowData, setRowData] = React.useState<Array<object>>([
-    { make: "Toyota", model: "Celica", price: 35000 },
-    { make: "Ford", model: "Mondeo", price: 32000 },
-    { make: "Porsche", model: "Boxster", price: 72000 }
-  ]);
-  const defaultColDef = {
-    sortable: true,
-    flex: 1,
-    filter: true,
-    floatingFliter: true,
-  }
+
   return (
     <div>
       <Header title={"hello"} />
-      <div
-        className="ag-theme-balham"
-        style={{
-          height: '100vh',
-          width: '100%'
-        }}
-      >
-        <AgGridReact
-          columnDefs={columnDefs}
-          rowData={rowData}
-          defaultColDef={defaultColDef}>
-        </AgGridReact>
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
