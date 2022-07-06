5f89a2c6a5c37a305f1edc2c9a15c00e5a5dfbce
Meet <rxt2jhhi@duck.com>
2022-07-06 11:50:05 +0530

    inserted data

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 import { AgGridReact } from 'ag-grid-react';
@@ -6,6 +6,7 @@ import { colors } from "../../common/color"
 import "./style.css"
 import TextField from "../../components/TextField/index"
 import AgGrid from "../../components/AgGrid";
+import data from "../../data.json";
 
 const HomePage = () => {
     const [columnDefs, setColumnDefs] = useState<Array<object>>([
@@ -21,17 +22,20 @@ const HomePage = () => {
         { headerName: "Status", field: "status", cellStyle: { 'background-color': colors.primary_light_grey, } },
         { headerName: "", field: "redrive", cellStyle: { 'background-color': colors.primary_light_grey, } },
     ]);
-    const [rowData, setRowData] = useState<Array<object>>([
-        { type: "Loan", id: "10002", tsla: "TSLA", quantity: "10,000", rate: "5.14%", counter_party: "Dhaval shah", create_date: "14/07/2022", settle_date: "09/10/2022", annotation: "Lorieam lissum", status: "Compalte", redrive: "" },
-        { type: "Borrow", id: "10014", tsla: "TSLA", quantity: "10,000", rate: "5.14%", counter_party: "momin mohammad", create_date: "09/10/2022", settle_date: "09/12/2022", annotation: "Lorieam lissum", status: "Compalte", redrive: "readdrive" },
-        { type: "Loan", id: "10035", tsla: "TSLA", quantity: "10,000", rate: "5.14%", counter_party: "Dhaval verariay", create_date: "14/08/2022", settle_date: "09/11/2022", annotation: "Lorieam lissum", status: "Compalte", redrive: "" }
-    ]);
+    const [rowData, setRowData] = useState<Array<object>>([...data.data]);
     const defaultColDef = {
         sortable: true,
         flex: 1,
         filter: true,
         floatingFliter: true,
     }
+
+    useEffect(() => {
+        data.data.map((item: any, index: any) => {
+            console.log("Data from json ", item)
+        })
+    }, [])
+
     return (
         <>
             <div className='textFieldDiv'>
