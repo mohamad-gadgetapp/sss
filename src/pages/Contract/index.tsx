03bdd621ee77c588ba5e4ad3053043e00f812d26
Kandarp <davekandarpc@gmail.com>
2022-07-18 12:49:20 +0530

    one file mache

--- src/pages/Contract/index.tsx
+++ src/pages/Contract/index.tsx
@@ -1,18 +1,16 @@
-import React, { InputHTMLAttributes, useState } from "react";
+import React, {useEffect, useState} from "react";
 import AgGrid from "../../components/AgGrid";
-import data from "../../contractsData.json";
 import "./style.css";
-import TextField from "../../components/TextField";
-import Button from "../../components/Button";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import SubHeader from "../../components/SubHeader";
+import * as XLSX from "xlsx";
+import data from "../../contractsData.json";
 
 interface ContractPageProps {
-  height?: number;
-  columns?: object;
+    height?: number;
+    columns?: object;
 }
 
-<<<<<<< HEAD
 const Contract = ({height}: ContractPageProps) => {
     const [rowData, setRowData] = useState<Array<object>>([...data.contractsData,]);
     const [heightAG_1] = useState(30);
@@ -54,8 +52,6 @@ const Contract = ({height}: ContractPageProps) => {
         };
         httpRequest.send();
     }
-
-    
     const convertDataToWorkbook = (dataRows: any) => {
         /* convert data to binary string */
         var data = new Uint8Array(dataRows);
@@ -106,66 +102,57 @@ const Contract = ({height}: ContractPageProps) => {
             Object.keys(columns).forEach(function (column) {
                 row[columns[column]] = worksheet[column + rowIndex].w;
             });
-=======
-const Contract = ({ height }: ContractPageProps) => {
-  const [rowData, setRowData] = useState<Array<object>>([
-    ...data.data,
-  ]);
-  const [heightAG_1, setHeightAG_1] = useState(30);
-  const [excelFile, setExcelFile] = useState("");
-  console.log("rowdata ",rowData)
->>>>>>> 83417d44efd7067df307f72cea6003fee9241334
 
-  const handleClick = (event: any) => {
-    console.log("handleClick " + JSON.stringify(event.target.files[0]));
-    setExcelFile(event.target.files[0]);
-  };
+            setRowData([...rowData_, row])
+            rowIndex++;
+        }
+    }
+    useEffect(() => {
+        console.log("rowData ", rowData)
+    }, [rowData])
 
-  const onSubmit = () => {
-    console.log("excelFile " + JSON.stringify(excelFile));
-  };
+    return (
+        <>
+            <div style={{height: `${height}rem`, overflow: "auto"}}>
+                <div className="subHeaderContract">
+                    <SubHeader title="Contract Booking"/>
+                </div>
+                <div className="react-grid-item-contract">
+                    <div className="contractBooking-Button">
+                        <div className="title-subHeaderTitle ">Contract</div>
+                        <div className="clear-submit-btn-div">
+                            <div className="upload-btn-main-div">
+                                <button className="excel-upload-btn">
+                                    <input type="file" className="FileUpload"
+                                           onChange={onChange}
+                                           accept=".xlsx,.xlsm,.xlsb,.xltx,.xltm,.xlt,.xls,.xls"/>
 
-  return (
-    <>
-      <div style={{ height: `${height}rem`, overflow: "auto" }}>
-        <div className="subHeaderContract">
-          <SubHeader title="Contract Booking" />
-        </div>
-        <div className="react-grid-item-contract">
-          <div className="contractBooking-Button">
-            <div className="title-subHeaderTitle ">Contract</div>
-            <div className="clear-submit-btn-div">
-              <div>
-                <button className="excel-upload-btn">
-                  <input
-                    type="file"
-                    className="FileUpload"
-                    onChange={(event) => handleClick(event)}
-                    accept=".xlsx,.xlsm,.xlsb,.xltx,.xltm,.xlt,.xls,.xls"
-                  />
-                  <div className="dropZoneOverlay">
-                    <AddCircleOutlineIcon
-                      style={{
-                        color: "grey",
-                        marginBottom: "0.25rem",
-                        marginRight: "0.5rem",
-                      }}
-                    />
-                    Upload Excel File
-                  </div>
-                </button>
-              </div>
-              <Button title="Submit" onClick={onSubmit} />
-              <button type="submit" className="clearAll-Btn">
-                Clear All
-              </button>
+                                    <div className="dropZoneOverlay">
+                                        <AddCircleOutlineIcon
+                                            style={{
+                                                color: "grey",
+                                                marginBottom: "0.25rem",
+                                                marginRight: "0.5rem",
+                                            }}
+                                        />
+                                        Upload Excel File
+                                    </div>
+                                </button>
+                                <div className="file-name">{excelFileName}</div>
+                            </div>
+                            <button type="submit" className="uploadSheet-submit-Btn" onClick={() => onSubmit()}>Submit
+                            </button>
+                            {/*<Button title="Submit" onClick={() => onSubmit()}/>*/}
+                            <button type="submit" className="clearAll-Btn" onClick={onClear}>
+                                Clear All
+                            </button>
+                        </div>
+                    </div>
+                    <AgGrid rowData={rowData} height={heightAG_1}/>
+                </div>
             </div>
-          </div>
-          <AgGrid rowData={rowData} height={heightAG_1} />
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default Contract;
